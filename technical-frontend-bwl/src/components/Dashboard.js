64ca288442ea11a4a7bf61a67ff5ada08030c09b
daan98 <daan98.da@gmail.com>
2022-06-10 23:58:06 -0500

    working on timezone

--- technical-frontend-bwl/src/components/Dashboard.js
+++ technical-frontend-bwl/src/components/Dashboard.js
@@ -1,35 +1,163 @@
-import React from "react";
+import axios from "axios";
+/* import {format, formatInTimeZone, zonedTimeRoUtc, zonedTimeToUtc} from 'date-fns-tz' */
+import enUS from 'date-fns/locale/en-US';
+import React, {useState, useEffect, Fragment} from "react";
 import Header from './Header';
 
 function Dashboard(){
-    const cloud = require("../assets/partly-cloudy-icon.png");
-    const flag = require("../assets/mexico.png");
-    /* let today = new Date();
-    let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds(); */
+    const [weather, setWeather] = useState([]);
+    const [clock, setClock] = useState('');
+    const cities = [
+        {
+            country: 'Mexico',
+            flag: require("../assets/mexico.png"),
+            timezone: ['Ciudad de México - Central', 'Tijuana', 'Monterrey'],
+            pais: 'México',
+        },
+        {
+            country: 'Argentina',
+            flag: require("../assets/argentina.png"),
+            timezone: ['Buenos Aires'],
+            pais: 'Argentina',
+        },
+        {
+            country: 'United States of America',
+            flag: require("../assets/eeuu.png"),
+            timezone: ['Pacífico - Nevada', 'Montaña - Colorado', 'Central - Oklahoma', 'Este - Virginia'],
+            pais: 'Estados Unidos',
+        },
+    ];
+    const key = 'abc55348044d4260bad201612220906';
+    let state = 'las vegas';
+    /* let time = new Date().toLocaleString('en-US', {hour: 'numeric', minute: 'numeric', second: 'numeric'});
+    let dateString = new Date().toLocaleString('en-GB', { day:'2-digit', month: '2-digit', year:'numeric' }); */
+    
+    const handleOnClickTimeZone = (e) => {
+        /* console.log(e); */
+        const timezones = document.querySelector('#timezone ul').querySelectorAll('li');
+        const time = new Date();
+        let hour = time.getHours()
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        let ampm = 'AM';
+
+        /* console.log(timezones); */
+        if(e.target.id === 'Pacifico - Nevada'){
+            state = 'las vegas';
+            hour -= 2;
+            /* setClock(TIME.toCorrectTimeZone()) */ // COLOCAR CONSTANTE 'time' EN SCOPE GLOBAL
+        } else if(e.target.id === 'Montaña - Colorado'){
+            state = 'denver';
+            hour -= 1;
+            /* setClock(TIME.toCorrectTimeZone()) */ // COLOCAR CONSTANTE 'time' EN SCOPE GLOBAL
+        } else if(e.target.id === 'Central - Oklahoma'){
+            state = 'oklahoma city';
+            /* setClock(TIME.toCorrectTimeZone()) */ // COLOCAR CONSTANTE 'time' EN SCOPE GLOBAL
+        } else if(e.target.id === 'Este - Virginia'){
+            state = 'virginia beach';
+            hour += 1;
+            /* setClock(TIME.toCorrectTimeZone()) */ // COLOCAR CONSTANTE 'time' EN SCOPE GLOBAL
+        } else if(e.target.id === 'Buenos Aires'){
+            state = 'buenos aires';
+            hour += 2;
+            /* setClock(TIME.toCorrectTimeZone()) */ // COLOCAR CONSTANTE 'time' EN SCOPE GLOBAL
+        } else{
+            setClock(time.toLocaleTimeString('en-US'));
+            return;
+        }
+
+        // Detectando cuando es de mañana o tarde
+        
+        if(hour > 12) {
+            hour -= 12
+            ampm = 'PM'
+        }else if(hour === 0) {
+            hour = 12
+            ampm = 'AM';
+        } else if(hour === 12) ampm = 'PM'
+
+        // Colocando un cero para mostrar siempre 2 digitos
+        if(hour.toString().length === 1) hour = `0${hour}`;
+        if(minutes.toString().length === 1) minutes = `0${minutes}`;
+        if(seconds.toString().length === 1) seconds = `0${seconds}`;
+
+        const timeString = `${hour}:${minutes}:${seconds} ${ampm}`;
+        /* time.toLocaleTimeString('en-US') */
+        setClock(timeString);
+    };
+    
+    // Use Effect para traer la información de weatherapi
+    useEffect( () => {
+        axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${state}&aqi=yes`)
+        .then( res => {
+            const data = [res.data];
+            setWeather(data);
+        })
+        .catch( error =>
+            console.log(error));
+    }, []);
+    
+        
+    // Use Effect para el reloj en tiempo real capaz de manejar sonas horarias
+    useEffect( () => {
+        setInterval(() => {
+            // const time = new Date();
+            /* let hour = time.getHours()
+            let minutes = time.getMinutes();
+            let seconds = time.getSeconds();
+            let ampm = 'AM'; */
+            
+            //PASAR handleOnClickTimeZone JUSTO AQUI
+            handleOnClickTimeZone();
+
+            /* // Detectando cuando es de mañana o tarde
+            if(hour > 12) {
+                hour -= 12
+                ampm = 'PM'
+            }else if(hour === 0) {
+                hour = 12
+                ampm = 'AM';
+            } else if(hour === 12) ampm = 'PM'
+
+            // Colocando un cero para mostrar siempre 2 digitos
+            if(hour.toString().length === 1) hour = `0${hour}`;
+            if(minutes.toString().length === 1) minutes = `0${minutes}`;
+            if(seconds.toString().length === 1) seconds = `0${seconds}`;
+
+            const timeString = `${hour}:${minutes}:${seconds} ${ampm}`;
+            // time.toLocaleTimeString('en-US')
+            setClock(timeString); */
+        }, 1000)
+    }, []);
+
 
     return(
         <div>
             <Header />
             <div className="grid container">
-                <div className="card">
-                    <h3>Clima</h3>
-                    <div id="weather" className="grid card-container" >
-                        <img srcSet={cloud} alt="weather"/>
-                        <div id="temperature">
-                            <p>29 C</p>
-                            <span>Parcialmente nublado</span>
+                { weather.map( info => (
+                    <Fragment>
+                        <div className="card">
+                            <h3>Clima</h3>
+
+                            <div id="weather" className="grid card-container" >
+                                <img srcSet={info.current.condition.icon} alt="weather"/>
+                                <div id="temperature">
+                                    <p>{info.current.temp_c} C</p>
+                                    <span>{info.current.condition.text}</span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
 
-                <div className="card">
-                    <h3>País seleccionado</h3>
-                    <div id="country" className="grid card-container" >
-                        <p>México</p>
-                        <img src={flag} alt="flag" />
-                    </div>
-                </div>
-
+                        <div className="card">
+                            <h3>País seleccionado</h3>
+                            <div id="country" className="grid card-container" >
+                                <p>{cities.filter(city => city.country === info.location.country )[0].pais}</p> {/* Filtrando valores en cities para mostrar el país seleccionado */}
+                                <img src={cities.filter(city => city.country === info.location.country )[0].flag} alt="flag" />
+                            </div>
+                        </div>
+                    
+                
                 <div className="card">
                     <h3>Tareas pendientes</h3>
                     <div id="uncompleted-task" className="card-container" >
@@ -44,7 +172,7 @@ function Dashboard(){
                 <div className="card">
                     <h3>Hora</h3>
                     <div id="hour" className="card-container" >
-                        <p>11:15:15 P.M.</p>
+                        <p>{clock}</p>
                     </div>
                 </div>
 
@@ -63,20 +191,30 @@ function Dashboard(){
                     <h3>Zonas horarias disponibles</h3>
                     <div id="timezone" className="card-container" >
                         <ul>
-                            <li>Ciudad de México - Central</li>
-                            <li>Tijuana</li>
-                            <li>Monterrey</li>
+                            { cities.map( city => {
+                                if(info.location.country === city.country){
+                                    return(
+                                        city.timezone.map( time => (
+                                            <li id={time} onClick={(e) => handleOnClickTimeZone(e)}>{time}</li>
+                                        ))
+                                    );
+                                } else return(false)
+                                }) }
                         </ul>
                     </div>
                 </div>
                 
                 <aside className="card">
                     <h3>Paises disponibles</h3>
-                    <div id="available-country" className="grid card-container" >
-                        <img src={flag} alt="flag" />
-                        <p>México</p>
-                    </div>
-                </aside> 
+                    { cities.map( city => (
+                        <div id="available-country" className="grid card-container" >
+                            <img src={city.flag} alt="flag" />
+                            <p>{city.pais}</p>
+                        </div>
+                    )) }
+                </aside>
+                </Fragment>
+                )) } 
             </div>
         </div>
     );
